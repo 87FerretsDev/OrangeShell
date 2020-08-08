e7eca6cf54d41660fb2e9740a845806fdb35952e
87FerretsDev <87ferrets@gmail.com>
2020-08-08 15:21:57 +0100

    Remove start.cmd as its no longer needed

--- UpdateManager/start.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-title Update OrangeOS
-set version_of_orangeos=%*
-python3 updater_main.py --version %version_of_orangeos%
-echo UpdateManager exited. Press any key to exit.
-pause>nul
-exit
